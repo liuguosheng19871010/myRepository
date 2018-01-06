648fe7a5f26db20104a695fb9f513afe8ffb53df
liuguosheng19871010 <785164951@qq.com>
2018-01-06 19:47:01 +0800

    commit 2018-01-06 19:46

--- src/com/example/main/MainDemo.java
+++ src/com/example/main/MainDemo.java
@@ -5,5 +5,6 @@ public class MainDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello 20180106-09:35");
 		System.out.println("Hello 20180106-19:45");
+		System.out.println("Hello 20180106-19:46");
 	}
 }
